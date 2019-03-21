3244c3d793027a78175755056b36c6d28deed9dc
李美丽 <304489471@qq.com>
2019-03-21 13:54:04 +0800

    歌手页面数据获取

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,14 +1,86 @@
 <template>
-  <div>
+  <div class="singer">
     歌手
   </div>
 </template>
 
 <script>
+  import {getSingerList} from 'src/api/singer'
+  import {ERR_OK} from 'src/api/config'
+  import Singer from 'common/js/singer'
+
+  const HOT_NAME = '热门'
+  const HOT_SINGER_LEN = 10
+
   export default {
-    name: 'singer'
+    name: 'singer',
+    data() {
+      return {
+        singers: []
+      }
+    },
+    created() {
+      this._getSingerList()
+    },
+    methods: {
+      _getSingerList() {
+        getSingerList().then((res) => {
+          if (res.code === ERR_OK) {
+            this.singers = res.data.list
+            console.log(this._normalizeSinger(this.singers))
+          }
+        })
+      },
+      _normalizeSinger(list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+        list.forEach((item, index) => {
+          if (index < HOT_SINGER_LEN) {
+            map.hot.items.push(new Singer({
+              id: item.Fsinger_mid,
+              name: item.Fsinger_name
+            }))
+          }
+          const key = item.Findex
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+          map[key].items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        })
+        // 为了得到有序列表需要处理map
+        let hot = [] // title是热门的歌手
+        let ret = [] // title是A-Z的歌手
+        for (let key in map) {
+          let val = map[key]
+          if (val.title.match(/[a-zA-Z]/)) {
+            ret.push(val)
+          } else if (val.title === HOT_NAME) {
+            hot.push(val)
+          }
+        }
+        ret.sort((a, b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+        })
+        return hot.concat(ret)
+      }
+    }
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus" scoped>
+  .singer
+    position fixed
+    top: 88px
+    bottom: 0
+    width 100%
 </style>
